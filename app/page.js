04c027517264333fb26ea3eb4418a4375ac34d94
chevyrider123 <tankzam@gmail.com>
2025-08-18 13:10:41 -0400

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👋 Hello, World!</h1>
+      <p>My Next.js site is finally working with the App Router 🎉</p>
+    </div>
+  );
+}
