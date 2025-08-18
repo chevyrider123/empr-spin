abb6daae452af55434c50bc98166ca7704cbbfb5
chevyrider123 <tankzam@gmail.com>
2025-08-18 13:02:23 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👋 Hello, World!</h1>
+      <p>My Next.js website is finally working 🎉</p>
+    </div>
+  );
+}
